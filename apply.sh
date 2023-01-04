8426e1c039ccb950d05d4b2e71e252af598cc2b8
Keiichi Tsuda <keiichi.tsuda@gmail.com>
2023-01-04 13:43:01 +0900

    add parameter file support

--- apply.sh
+++ apply.sh
@@ -8,6 +8,7 @@ if [ $# -lt 1 ]; then
 fi
 
 TEMPLATE=$1-template.yaml
+PARAMS=$1-parameters.json
 STACK=$1-stack
 
 if [ ! -e templates/${TEMPLATE} ]; then
@@ -28,16 +29,29 @@ chk=$(aws --profile ${PROFILE} cloudformation list-stacks \
       | jq -r '.StackSummaries[].StackName' | grep ${STACK} | wc -l)
 
 # create/update stack
+alias create-stack="aws --profile ${PROFILE} cloudformation create-stack --stack-name ${STACK} --template-body file://templates/${TEMPLATE} --capabilities CAPABILITY_IAM"
+alias update-stack="aws --profile ${PROFILE} cloudformation update-stack --stack-name ${STACK} --template-body file://templates/${TEMPLATE} --capabilities CAPABILITY_IAM"
 if [ 1 -ne ${chk} ]; then
   # create
   echo create-stack ...
-  aws --profile ${PROFILE} cloudformation create-stack --stack-name ${STACK} --template-body file://templates/${TEMPLATE} 
+  if [ -e parameters/${PARAMS} ]; then
+    MINIFIED_PARAMS=$(jq -c < parameters/${PARAMS})
+    create-stack --parameters "${MINIFIED_PARAMS}"
+  else
+    create-stack
+  fi
 else
   # update
   echo update-stack ...
-  aws --profile ${PROFILE} cloudformation update-stack --stack-name ${STACK} --template-body file://templates/${TEMPLATE}
+  if [ -e parameters/${PARAMS} ]; then
+    MINIFIED_PARAMS=$(jq -c < parameters/${PARAMS})
+    update-stack --parameters "${MINIFIED_PARAMS}"
+  else
+    update-stack
+  fi
 fi
 
+# check exit code
 if [ $? -eq 0 ]; then
   echo success
   exit 0
